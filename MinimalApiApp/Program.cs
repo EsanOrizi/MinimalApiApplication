a922f78edf4e29f69be0dd23afef7ae0aefce1f5
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2025-02-27 11:08:29 +0000

    Adding real endpoint to get data from database

--- MinimalApiApp/Program.cs
+++ MinimalApiApp/Program.cs
@@ -1,9 +1,14 @@
+using TodoLibrary.DataAccess;
+using TodoLibrary.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<ITodoData, TodoData>();
 
 var app = builder.Build();
 
@@ -15,8 +20,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapGet("/api/Todo", () => new string[] { "string1", "string2" });
-app.MapGet("/api/Todo/{id}", (int id) => $"Id: {id}");
+
+app.MapGet("/api/Todos", async (ITodoData data) =>
+{
+    var output = await data.GetAllAssigned(1);
+    return Results.Ok(output);
+});
 
 app.Run();
 
